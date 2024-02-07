8895e44ef59fe8a7802a232b6f19cc0480fdf108
Kenneth Yee <kennbyee25@gmail.com>
2024-02-07 16:41:58 -0500

    correct bulb init

--- scripts/sunrise.py
+++ scripts/sunrise.py
@@ -13,12 +13,20 @@ if __name__ == '__main__':
     end_temp = config['end_temp']
     duration = config['duration']
     steps = config['steps']
-    
+
     bulb = yeelight.Bulb(bulb_ip)
+    properties = bulb.get_properties()
+    bulb_is_on = properties['power'] == 'on'
+
+    if not bulb_is_on:
+        bulb.turn_on()
+        bulb.set_color_temp(start_temp)
+        bulb.set_brightness(1)
     
     for i in range(steps+1):
         temp = start_temp + (end_temp - start_temp) * i / steps
         brightness = int(100 * i / steps)
         bulb.set_color_temp(temp)
         bulb.set_brightness(brightness)
+        
         time.sleep(duration / steps)
